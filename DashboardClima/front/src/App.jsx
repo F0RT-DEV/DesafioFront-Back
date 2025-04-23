f8637b90ff0a5907a24e7369a7126c36d281775b
DOUGLAS FERREIRA BORGES <d.borges@labcetec.net>
2025-04-23 10:41:46 -0300

    Subindo o projeto

---
+++ DashboardClima/front/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+  <div className="entry-portal">
+  <h1 className="title-glow">🌍 ClimaVerse</h1>
+  <p className="subtitle-fade">Explore o clima como nunca antes</p>
+  <button className="enter-button">Entrar no ClimaVerse</button>
+</div>
+
+    </>
+  )
+}
+
+export default App
